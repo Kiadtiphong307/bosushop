1c80d56c207dfb24ca604b16e14a614b9d806bc7
Kiaditphong307 <mju6504101307@mju.ac.th>
2025-05-21 11:34:29 +0700

    Refactor backend structure and enhance database initialization
    - Updated .env file for consistent formatting.
    - Refactored Dockerfile for backend to optimize layer caching.
    - Enhanced air.toml for better build configuration.
    - Improved main.go to streamline database initialization and seeding process.
    - Updated various controller and service files for better organization and clarity.
    - Enhanced validation logic for order and product inputs.
    - Improved routing structure for categories, coupons, and orders.
    - Added new features for coupon management and product handling.

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-
-
 	"github.com/gofiber/fiber/v2"
 
 	"backend/database"
 	"backend/routes"
 	"backend/seed"
+	"fmt"
 )
 
 func main() {
@@ -17,14 +16,15 @@ func main() {
 	database.InitDatabase()
 
 	// เรียกใช้ routes ทั้งหมด
-	routes.AuthRoutes(app) // สำหรับการสมัครสมาชิกและเข้าสู่ระบบ
-	routes.OrderRoutes(app) // สำหรับการสร้างคำสั่งซื้อ
-	routes.ProductRoutes(app) // สำหรับการสร้างสินค้า
+	routes.AuthRoutes(app)     // สำหรับการสมัครสมาชิกและเข้าสู่ระบบ
+	routes.OrderRoutes(app)    // สำหรับการสร้างคำสั่งซื้อ
+	routes.ProductRoutes(app)  // สำหรับการสร้างสินค้า
 	routes.CategoryRoutes(app) // สำหรับการสร้างหมวดหมู่สินค้า Admin
-	routes.CouponRoutes(app) // สำหรับการสร้างคูปอง
+	routes.CouponRoutes(app)   // สำหรับการสร้างคูปอง
 
 	// เรียกใช้ seed ข้อมูล
 	seed.InitSeeder(database.DB)
+
 
 	app.Listen(":8080")
 }
